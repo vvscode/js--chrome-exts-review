26aea864ed89ebb3848fbe049c5809706583b45d
Vasil Vanchuk <v.vanchuk@tut.by>
2015-09-27 14:51:09 +0300

    Add panes

--- devtools/devtools.js
+++ devtools/devtools.js
@@ -4,3 +4,19 @@ chrome.devtools.panels.create(
   "panel/panel.html",
   null
 );
+
+chrome.devtools.panels.elements.createSidebarPane('My element pane', function(pane) {
+  pane.setPage('pane_for_elements/pane.html');
+});
+
+chrome.devtools.panels.sources.createSidebarPane('My sources pane', function(pane){
+  pane.setPage('pane_for_sources/pane.html');
+  pane.setHeight('200px');
+  pane.onShown.addListener(function() {
+    pane.setExpression('(' + getAllTheDivs.toString() + ')()');
+  });
+});
+
+function getAllTheDivs() {
+  return [].slice.call(document.getElementsByTagName('div'));
+}
